2938b6da8abdc58a0b14d1ce054330e801a00725
Adrià Freixas Grimalt <adria.freixas@webbeds.com>
2023-03-22 11:32:29 +0100

    prints sum

--- GradeAverageCalculator/Program.cs
+++ GradeAverageCalculator/Program.cs
@@ -22,7 +22,12 @@ int jeong3 = 98;
 int jeong4 = 100;
 int jeong5 = 97;
 
-Console.WriteLine("Sophia: " + sophia1 +", "+ sophia2 +", "+ sophia3 +", "+ sophia4 +", "+ sophia5);
-Console.WriteLine("Nicolas: " + nicolas1 +", "+ nicolas2 +", "+ nicolas3 +", "+ nicolas4 +", "+ nicolas5);
-Console.WriteLine("Zahirah: " + zahirah1 +", "+ zahirah2 +", "+ zahirah3 +", "+ zahirah4 +", "+ zahirah5);
-Console.WriteLine("Jeong: " + jeong1 +", "+ jeong2 +", "+ jeong3 +", "+ jeong4 +", "+ jeong5);
+int sophiaSum = sophia1 + sophia2 + sophia3 + sophia4 + sophia5;
+int nicolasSum = nicolas1 + nicolas2 + nicolas3 + nicolas4 + nicolas5;
+int zahirahSum = zahirah1 + zahirah2 + zahirah3 + zahirah4 + zahirah5;
+int jeongSum = jeong1 + jeong2 + jeong3 + jeong4 + jeong5;
+
+Console.WriteLine("Sophia: " + sophiaSum);
+Console.WriteLine("Nicolas: " + nicolasSum);
+Console.WriteLine("Zahirah: " + zahirahSum);
+Console.WriteLine("Jeong: " + jeongSum);
